11ca6f452be7e03b4ebb6adce19bcfabd9f4b34a
Dax Hurley <dax.orion.hurley@gmail.com>
2021-05-13 18:28:08 -0600

    Added pagination to hashtag pages

--- controllers/hashtagController.js
+++ controllers/hashtagController.js
@@ -3,8 +3,13 @@ const User = require("../models/user");
 module.exports = {
     show: (req, res, next) => {
         let hashtag = req.params.hashtag;
-        Post.find({ hashtags: { $in: [hashtag] }}).sort({createdAt: 'desc'}).populate({path: 'user'}).then(posts => {
+        Post.find({ hashtags: { $in: [hashtag] }})
+        .limit(res.locals.itemCount)
+        .skip(res.locals.skipCount)
+        .sort({createdAt: 'desc'})
+        .populate({path: 'user'}).then(posts => {
             res.locals.posts = posts;
+            res.locals.hashtag = hashtag;
             next();
         })
     },
--- routes/hashtagRoutes.js
+++ routes/hashtagRoutes.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const hashtagController = require("../controllers/hashtagController");
+const commonController = require("../controllers/commonController");
 
-// Hashtags
-router.get("/:hashtag", hashtagController.show, hashtagController.showView);
+router.get("/:hashtag", commonController.paginationHandler, hashtagController.show, hashtagController.showView);
+router.get("/:hashtag/page/:pageNum", commonController.paginationHandler, hashtagController.show, hashtagController.showView);
 
 module.exports = router;
